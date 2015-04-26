c69584fae10c91179778a7c9dbff7a79e27c6e15
IanzTristian <ianztristian@gmail.com>
2015-04-25 20:47:01 -0700

    Demo swicth

---
+++ DemoSwitch/DemoSwitch/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TesSwicth();
+        }
+        static void TesSwicth()
+        {
+            Console.WriteLine("Pilih Bahasa Pemrograman");
+            Console.WriteLine("************************");
+            Console.WriteLine("1. C#");
+            Console.WriteLine("2. VB.NET");
+            Console.WriteLine("3. PHP");
+
+            Console.Write("\nMasukkan Pilihan Anda : ");
+            int pilihan = Convert.ToInt32(Console.ReadLine());
+
+            var keterangan = string.Empty;
+
+            switch (pilihan)
+            {
+                case 1:
+                    keterangan = "C";
+                    break;
+
+                case 2:
+                    keterangan = "VB.NET";
+                    break;
+
+                case 3:
+                    keterangan = "PHP";
+                    break;
+                default:
+                    keterangan = "Pilihan anda tidak ada";
+                    break;
+            }
+            Console.WriteLine("Anda Memilih : {0}\n", keterangan);
+        }
+    }
+}
