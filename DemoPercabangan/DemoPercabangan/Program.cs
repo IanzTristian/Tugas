789909d9c5f8b241f72f407c48b50def771b4ed8
IanzTristian <ianztristian@gmail.com>
2015-04-29 06:48:10 -0700

    Latihan Percabangan

---
+++ DemoPercabangan/DemoPercabangan/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoPercabangan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TesIfTunggal();
+            //TesIfDenganTernary();
+            //TesIfMajemuk();
+            TesIfMajemuk2();
+        }
+        static void TesIfTunggal()
+        {
+            Console.Write("Nilai Anda");
+            int nilai = Convert.ToInt32(Console.ReadLine());
+
+            var keterangan = string.Empty;
+
+            if (nilai >= 60)
+                keterangan = "Anda Lulus";
+            else
+                keterangan = "Anda Tidak Lulus";
+
+            Console.WriteLine(keterangan + "\n");
+        }
+
+        static void TesIfDenganTernary()
+        {
+            Console.Write("Nilai Anda : ");
+            int nilai = Convert.ToInt32(Console.ReadLine());
+
+            var keterangan = nilai >= 60 ? "Anda Lulus" : "Anda Tidak Lulus";
+
+            Console.WriteLine(keterangan + "\n");
+        }
+        static void TesIfMajemuk()
+        {
+            Console.Write("Masukkan Nilai Angka : ");
+            int nilai = Convert.ToInt32(Console.ReadLine());
+
+            var nilaiHuruf = string.Empty;
+
+            if (nilai >= 80 && nilai <= 100)
+                nilaiHuruf = "A";
+            else if (nilai >= 70)
+                nilaiHuruf = "B";
+            else if (nilai >= 60)
+                nilaiHuruf = "C";
+            else if (nilai >= 45)
+                nilaiHuruf = "D";
+            else
+                nilaiHuruf = "E";
+
+            Console.WriteLine("Nilai Huruf : {0}\n", nilaiHuruf);
+        }
+        static void TesIfMajemuk2()
+        {
+            Console.WriteLine("Pilih Bahasa Pemrograman");
+            Console.WriteLine("************************");
+            Console.WriteLine("1. C#");
+            Console.WriteLine("2. VB.NET");
+            Console.WriteLine("3. PHP");
+
+            Console.Write("\nMasukkan Pilihan Anda : ");
+            int pilihan = Convert.ToInt32(Console.ReadLine());
+
+            var keterangan = string.Empty;
+
+            if (pilihan == 1)
+                keterangan = "C#";
+            else if (pilihan == 2)
+                keterangan = "VB.NET";
+            else if (pilihan == 3)
+                keterangan = "PHP";
+            else
+                keterangan = "Pilihan anda tidak ada";
+            Console.WriteLine("Anda Memilih : {0}\n", keterangan);
+    
+        }
+    }
+}
---
+++ DemoSukuBungaPelanggan/DemoSukuBungaPelanggan/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoSukuBungaPelanggan
+{
+    class Program
+    {
+        static void Main(string[] args)
+    {
+        DemoSukuBungaPelanggan();
+    }
+   
+        
+        static void DemoSukuBungaPelanggan()
+        {
+            Console.Write("Masukkan Peringkat anda : ");
+            string Peringkat = Console.ReadLine().ToUpper();
+
+            float SukuBunga = 0.00F;
+
+            if (Peringkat == "A" || Peringkat == "B")
+                SukuBunga = 0.11F;
+            else if (Peringkat == "C")
+                SukuBunga = 0.15F;
+            else if (Peringkat == "D")
+                SukuBunga = 0.17F;
+            else if (Peringkat == "E")
+                SukuBunga = 0.19F;
+            else
+                SukuBunga = 0.21F;
+
+            Console.WriteLine("Suku Bunga Anda adalah: {0}%\n", SukuBunga * 100);
+        }
+    }
+}
