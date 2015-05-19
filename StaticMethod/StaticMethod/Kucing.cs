83b2b40a0acd74808dfc05508b218eb5e173fd4d
IanzTristian <ianztristian@gmail.com>
2015-05-19 05:53:32 -0700

    Static Method

---
+++ StaticMethod/StaticMethod/Kucing.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethod
+{
+    public class Kucing
+    {
+        private static int instances = 0;
+
+        //Constructor : Method Khusus
+        public Kucing()
+        {
+            instances++;
+        }
+
+        public static void BanyakKucing()
+        {
+           Console.WriteLine("Banyak Kucing : (0)", instances);
+        }
+    }
+}
---
+++ StaticMethod/StaticMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kucing kucing1 = new Kucing();
+            Kucing.BanyakKucing();
+
+            Kucing kucing2 = new Kucing();
+            Kucing kucing3 = new Kucing();
+
+            Kucing.BanyakKucing();
+
+        }
+    }
+}
