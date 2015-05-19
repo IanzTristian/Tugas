fbd0c0aa5f27f846da16e7059fda41c30d7c5f65
IanzTristian <ianztristian@gmail.com>
2015-05-19 04:32:44 -0700

    Latihan Method

---
+++ LatihanMethod/LatihanMethod/MethodVoid.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMethod
+{
+   public class MethodVoid
+    {
+       public void HelloWorld()
+         {
+                Console.WriteLine("Hello World");
+         }
+        public void InfoBuku(string Judul, String Pengarang)
+         {
+                Console.WriteLine("Judul {0}, Dibuat Oleh {1}", Judul, Pengarang);
+         }     
+    }
+   
+    public class MethodNonVoid
+    {
+        public int penjumlahan(int bil1, int bil2)
+        {
+            return bil1 + bil2;
+        }
+
+        public double LuasSegitiga(double alas, double tinggi)
+        {
+            double luas = 0.5 * alas * tinggi;
+            return luas;
+        }
+    }
+}
---
+++ LatihanMethod/LatihanMethod/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMethod
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //membuat object MethoidVoid
+            MethodVoid mv = new MethodVoid();
+
+            //invoke/panggilan method
+            mv.HelloWorld();
+            mv.InfoBuku("C# Fundamental", "Rudi");
+
+            //membuat object MethodVoid
+            MethodNonVoid mnv = new MethodNonVoid();
+
+            //invoke pemanggilan method
+            int hasil = mnv.penjumlahan(10, 5);
+            Console.WriteLine("Hasil : {0}", hasil);
+
+            double luas = mnv.LuasSegitiga(10, 5);
+            Console.WriteLine("Hasil : {0}", luas);
+
+
+            Console.ReadKey();
+        }
+    }
+}
