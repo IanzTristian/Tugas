d73d79a5fb5c70d597feba94d0a7dc2a06819f74
IanzTristian <ianztristian@gmail.com>
2015-05-19 04:33:26 -0700

    Latihan Method2

---
+++ LatihanMethod2/LatihanMethod2/ParameterClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMethod2
+{
+   public class ParameterClass
+   {
+        //method by value
+       public int Kali(int bil1, int bil2)
+       {
+           int hasil = bil1 * bil2;
+           bil1 += 2;
+           bil2 *= 2;
+
+           return hasil;
+       }
+       //method by ref
+       public int tambah (ref int bil1, ref int bil2)
+       {
+           int hasil = bil1 + bil2;
+           bil1 += 2;
+           bil2 *= 2;
+
+           return hasil;
+       }
+
+       //output paramaters
+       public int Kurang (int bil1, int bil2, out string tipe)
+       {
+           int hasil = bil1 - bil2;
+           if (hasil < 0)
+               tipe = "negatif";
+           else
+               tipe = "positif";
+
+           return hasil;
+       }
+   }
+}
---
+++ LatihanMethod2/LatihanMethod2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMethod2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 2;
+            ParameterClass pc = new ParameterClass();
+            /*
+            int hasil = pc.Kali(a, b);
+            Console.WriteLine("Hasil : {0}", hasil);
+            Console.WriteLine("Bilangan 1 : {0}", a);
+            Console.WriteLine("Bilangan 2 : {0}", b);
+            */
+
+            /*
+            int hasil = pc.tambah(ref a, ref b);
+            Console.WriteLine("Hasil  {0}", hasil);
+            Console.WriteLine("Bilangan 1 : {0}", a);
+            Console.WriteLine("Bilangan 2 : {0}", b);
+            */
+            string tipe = string.Empty;
+            int hasil = pc.Kurang(a, b, out tipe);
+            Console.WriteLine("Hasil : {0}", hasil);
+            Console.WriteLine("Tipe : {0}", tipe);
+
+            Console.ReadKey();
+        }
+    }
+}
