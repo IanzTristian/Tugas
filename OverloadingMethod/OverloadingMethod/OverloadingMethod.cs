4ec26a32212cc5b0c46ab2b06213630e761b5893
IanzTristian <ianztristian@gmail.com>
2015-05-19 05:52:35 -0700

    Overloading Method

---
+++ OverloadingMethod/OverloadingMethod/OverloadingMethod.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingMethod
+{
+    public class OverloadingMethod
+    {
+        public string HelloWorld()
+        {
+            return "Hello World !!";
+        }
+
+        public string HelloWorld(string pesan)
+        {
+            return pesan;
+        }
+        public string HelloWorld(string pesan, string user)
+        {
+            return pesan + "\tOleh : " + user;
+        }
+        public int HelloWorld(int status)
+        {
+            return status;
+        }
+    }
+
+}
---
+++ OverloadingMethod/OverloadingMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OverloadingMethod om = new OverloadingMethod();
+
+            Console.WriteLine(om.HelloWorld());
+            Console.WriteLine(om.HelloWorld("Hello C#"));
+            Console.WriteLine(om.HelloWorld("Hello C#", "Andi"));
+
+            Console.WriteLine(om.HelloWorld(2));
+        }
+    }
+}
